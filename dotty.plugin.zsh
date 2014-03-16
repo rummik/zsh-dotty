f59b66240f11f0707ffa47b3a0d695394dcf4cd3
rummik <r@9k1.us>
2014-03-15 20:58:56 -0400

    Remove extra parens from function defines

--- dotty.plugin.zsh
+++ dotty.plugin.zsh
@@ -1,28 +1,28 @@
 #!/bin/zsh
 
-function dotty-track() {
+function dotty-track {
 	dotty-git add -f $@
 	dotty-git commit -m "Tracked $@"
 }
 
-function dotty-untrack() {
+function dotty-untrack {
 	dotty-git rm --cached $@
 	dotty-git commit -m "Untracked $@"
 }
 
-function dotty-rm() {
+function dotty-rm {
 	dotty-untrack $@
 }
 
-function dotty-add() {
+function dotty-add {
 	dotty-track $@
 }
 
-function dotty-status() {
+function dotty-status {
 	dotty-git status
 }
 
-function dotty-remote() {
+function dotty-remote {
 	if [[ -z "$(dotty-git remote show -n origin | grep /$1.git)" ]]; then
 		dotty-git remote add -t master origin git://github.com/$1.git
 		dotty-git remote set-url --push origin git@github.com:$1.git
@@ -31,30 +31,30 @@ function dotty-remote() {
 	fi
 }
 
-function dotty-sync() {
+function dotty-sync {
 	dotty-git commit -am 'Updating tracked files'
 	dotty-git pull --rebase origin
 	dotty-git push origin
 }
 
-function dotty-diff() {
+function dotty-diff {
 	dotty-git diff
 }
 
-function dotty-git() {
+function dotty-git {
 	git --git-dir=$HOME/.dotty --work-tree=$HOME $@
 }
 
-function dotty-ls() {
+function dotty-ls {
 	dotty-git ls-files
 }
 
-function dotty-init() {
+function dotty-init {
 	dotty-git init
 	print '/*\n!.*\n*~\n.config\n.netrc\n.*_userrc\n.*_history\n.ssh/id_rsa*\n!.ssh/id_rsa*.pub' >> ~/.dotty/info/exclude
 }
 
-function dotty() {
+function dotty {
 	local cmd=$1; [[ $# -ge 1 ]] && shift
 	[[ ! -d ~/.dotty ]] && dotty-init
 
